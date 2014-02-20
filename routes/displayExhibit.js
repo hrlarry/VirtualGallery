a739082974a52b65b8544bbcaf6efcc46e2b0f9c
vagrant <vagrant@precise32.(none)>
2014-02-20 04:21:32 +0000

    changed routes from fakeDatabase to real database

--- routes/displayExhibit.js
+++ routes/displayExhibit.js
@@ -1,14 +1,35 @@
-var profiles = require('../fakeDatabase.json');
+//var profiles = require('../fakeDatabase.json');
+
+var models = require('../models');
 
 exports.exhibitInfo = function(req, res) { 
 	var exhibitID = req.params.id;
-	if (exhibitID == "random") {
-		exhibitID = Math.floor(Math.random() * profiles.users[0].exhibits.length) + 1;
-	} else {
-		exhibitID = parseInt(exhibitID);
+
+	//get exhibits for the current user - right now, user 0
+	models.User
+		.find()
+		.exec(showExhibit);
+
+	function showExhibit(err, users) {
+		if (exhibitID == "random") {
+			exhibitID = Math.floor(Math.random() * users[0].exhibits.length) + 1;
+		} else {
+			exhibitID = parseInt(exhibitID);
+		}
+
+		//console.log("from displayExhibit: "+users[0]);
+  		var exhibitToShow = users[0].exhibits[exhibitID-1]; // of by one, our first project has index 0
+  		res.json(exhibitToShow);
 	}
 
-	//console.log("from displayExhibit: "+users[0]);
-  	var exhibitToShow = profiles.users[0].exhibits[exhibitID-1]; // of by one, our first project has index 0
-  	res.json(exhibitToShow);
+
+	// if (exhibitID == "random") {
+	// 	exhibitID = Math.floor(Math.random() * profiles.users[0].exhibits.length) + 1;
+	// } else {
+	// 	exhibitID = parseInt(exhibitID);
+	// }
+
+	// //console.log("from displayExhibit: "+users[0]);
+ //  	var exhibitToShow = profiles.users[0].exhibits[exhibitID-1]; // of by one, our first project has index 0
+ //  	res.json(exhibitToShow);
 }
