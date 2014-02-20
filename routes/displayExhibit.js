2db5cf7292c153835826c04811357ae6f89f9d5c
penguinfrk <hrlarry@stanford.edu>
2014-02-19 23:16:15 -0800

    changed exhibit viewing pages to support new database structure.

--- routes/displayExhibit.js
+++ routes/displayExhibit.js
@@ -3,12 +3,12 @@ var profiles = require('../fakeDatabase.json');
 exports.exhibitInfo = function(req, res) { 
 	var exhibitID = req.params.id;
 	if (exhibitID == "random") {
-		exhibitID = Math.floor(Math.random() * profiles.users[0].exhibits.length) + 1;
+		exhibitID = Math.floor(Math.random() * profiles[0].exhibits.length) + 1;
 	} else {
 		exhibitID = parseInt(exhibitID);
 	}
 
 	//console.log("from displayExhibit: "+users[0]);
-  	var exhibitToShow = profiles.users[0].exhibits[exhibitID-1]; // of by one, our first project has index 0
+  	var exhibitToShow = profiles[0].exhibits[exhibitID-1]; // of by one, our first project has index 0
   	res.json(exhibitToShow);
 }
--- routes/visitGallery.js
+++ routes/visitGallery.js
@@ -1,5 +1,5 @@
 var profiles = require('../fakeDatabase.json');
 
 exports.displayPage = function(req, res){
-  res.render('visitGallery', profiles.users[0].exhibits[0]);
+  res.render('visitGallery', profiles[0].exhibits[0]);
 }
